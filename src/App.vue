8d7d89133c7cc3334e4f2b13aed98668c48de568
Emma <129emma@gmail.com>
2018-02-21 15:58:26 +1300

    Adding files.

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
+
+    <web-context>
+    </web-context>
+
+    <web-footer>
+    </web-footer>
+
+  </div>
+</template>
+
+<script>
+  import footer from './components/Footer.vue';
+  import context from './components/Context.vue';
+
+  export default {
+    components:{
+      'web-context': context,
+      'web-footer' : footer
+    }
+
+  }
+</script>
+
+<style>
+
+  /* Links */
+  a,
+  a:focus,
+  a:hover {
+    color: #fff;
+  }
+
+  /*
+  * Base structure
+  */
+
+  html,
+  body {
+    height: 100%;
+    background-color: #333;
+  }
+
+  body {
+    display: -ms-flexbox;
+    display: -webkit-box;
+    display: flex;
+    -ms-flex-pack: center;
+    -webkit-box-pack: center;
+    justify-content: center;
+    color: #fff;
+    text-shadow: 0 .05rem .1rem rgba(0, 0, 0, .5);
+    box-shadow: inset 0 0 5rem rgba(0, 0, 0, .5);
+    background-image: url("./assets/background.jpg");
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+
+  .cover-container {
+    max-width: 42em;
+  }
+
+
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer class="mastfoot mt-auto">
+    <div class="inner">
+      <p>This page is created by <a href="https://twitter.com/lelesay">@エツ</a></p>
+    </div>
+  </footer>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+p {
+  margin-bottom: 0;
+  font-size: .8em;
+}
+p, a{
+  color: rgba(255, 255, 255, 0.6);
+}
+
+</style>
